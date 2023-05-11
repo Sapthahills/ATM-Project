9b55bfb6ea2005758d42dbefb1e67053e92a6c14
Sapthagiri <123750505+Sapthahills@users.noreply.github.com>
2023-05-11 15:39:58 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+package ATM;
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("\nWelcome to our ATM");
+        card.card();
+        dot d=new dot();
+        d.run();
+
+        Options opt=new Options();
+        opt.savings();
+
+    }
+}
